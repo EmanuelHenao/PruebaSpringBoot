707efe7190cecb95975c2671076e5f8f186e8622
EmanuelHenao <emanuelhenaogiraldo@gmail.com>
2022-05-23 01:27:58 -0500

    Feat[DEV-M3]: moduloUserTask

---
+++ pruebaTecnica/serviceUserTask-m3/src/main/java/com/PruebaTecnica/Springboot/serviceUserTask/service/UserTareaService.java
@@ -0,0 +1,10 @@
+package com.PruebaTecnica.Springboot.serviceUserTask.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserTareaService {
+
+}
